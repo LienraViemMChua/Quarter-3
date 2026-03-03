29d9a45ae0422193045022828297be4af8d3d2c3
LienraViemMChua <Lienraviem.chua@smc.pshs.edu.ph>
2026-03-03 23:06:48 +0800

    Create Chua_Q3_FA06.py

---
+++ Chua_Q3_FA06.py
@@ -0,0 +1,13 @@
+# Create an empty dictionary
+stud = {}
+
+# Collect student information
+stud["name"] = input("Enter your name: ")
+stud["age"] = input("Enter your age: ")
+stud["subj"] = input("Enter your favorite subject: ")
+
+# Display the student record
+print("\nStudent Record:")
+print("Name:", stud["name"])
+print("Age:", stud["age"])
+print("Favorite Subject:", stud["subj"])
